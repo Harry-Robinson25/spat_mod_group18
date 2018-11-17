a49061f2622ca846f3a73a0cd83be72e5f17cdb5
Harry Robinson <harry@popocatapetl.local>
2018-11-17 15:48:39 +0000

    added missing packages to setup script

--- setup_environment.R
+++ setup_environment.R
@@ -2,6 +2,11 @@ DATAFILE <- '~/code/spat_mod_group18/data/HadCRUT.median.nc'
 
 install.packages('tidyverse')
 install.packages('ncdf4')
+install.packages('raster')
+install.packages('rasterVis')
+install.packages('maptools')
+install.packages('maps')
+
 download.file(
   url='https://crudata.uea.ac.uk/cru/data/temperature/HadCRUT.4.6.0.0.median.nc',
   destfile=DATAFILE,
