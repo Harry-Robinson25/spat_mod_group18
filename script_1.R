2f1343ea26b9e1b965393fe208b6970f1a29105c
Harry Robinson <harry@popocatapetl.local>
2018-11-17 15:07:21 +0000

    initial script :wq

---
+++ script_1.R
@@ -0,0 +1,18 @@
+setwd("~/Desktop/Spatial Modelling ")
+library(raster)
+library(ncdf4)
+library(rasterVis)
+library(maptools)
+library(maps)
+CLIMATE = stack("HadCRUT.4.6.0.0.median.nc")
+CLIMATE
+CLIMATE[[1]]
+image(CLIMATE[[1]])
+levelplot(CLIMATE[[1]])
+mapTheme = rasterTheme(region=rainbow(256))
+levelplot(CLIMATE[[1]],par.settings=mapTheme)
+summary(CLIMATE[[1]])
+world.outlines=map("world",plot=FALSE)
+world.outlines.sp= map2SpatialLines(world.outlines,proj4string= CRS("+proj=longlat"))
+xx1 = levelplot(CLIMATE[[1]],par.settings=mapTheme)
+xx1 + layer(sp.lines(world.outlines.sp,col="black",lwd=0.5))
