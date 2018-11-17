66267db72ef9f3de0de35faa358c54d35e09c36f
Harry Robinson <harry@popocatapetl.local>
2018-11-17 15:26:01 +0000

    added setup script

---
+++ setup_environment.R
@@ -0,0 +1,10 @@
+DATAFILE <- '~/code/spat_mod_group18/data/HadCRUT.median.nc'
+
+install.packages('tidyverse')
+install.packages('ncdf4')
+download.file(
+  url='https://crudata.uea.ac.uk/cru/data/temperature/HadCRUT.4.6.0.0.median.nc',
+  destfile=DATAFILE,
+)
+
+
